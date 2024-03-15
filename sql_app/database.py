437d7987c5c669791987974f0dea2ee1c2b0b0c6
Abhinav M Hari <abhinav03m@gmail.com>
2024-03-15 10:43:12 +0530

    Fixed error in the DATABASE_URL

--- sql_app/database.py
+++ sql_app/database.py
@@ -8,11 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # sqlite db for testing
 # DATABASE_URL = "sqlite:///./wics.db"
-DATABASE_URL = "postgres://default:jSJg6x8vfXuL@ep-round-mouse-a1oo2jy0-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+DATABASE_URL = "postgresql://default:jSJg6x8vfXuL@ep-round-mouse-a1oo2jy0-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
 
 # connect_args only required for sqlite
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
