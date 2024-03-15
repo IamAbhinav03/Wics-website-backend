e2ab2743b45bacd113b9908e055a2739d993688c
Abhinav M Hari <abhinav03m@gmail.com>
2024-03-15 10:38:00 +0530

    Updated db url to use vercel postgress

--- sql_app/database.py
+++ sql_app/database.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # sqlite db for testing
-DATABASE_URL = "sqlite:///./wics.db"
+# DATABASE_URL = "sqlite:///./wics.db"
+DATABASE_URL = "postgres://default:jSJg6x8vfXuL@ep-round-mouse-a1oo2jy0-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
 
 # connect_args only required for sqlite
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
