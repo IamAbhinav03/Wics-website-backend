c9e8be1853617a8d12b1c088773edc6ba96f4ced
Abhinav M Hari <abhinav03m@gmail.com>
2024-03-15 18:47:52 +0530

    Giving access to all clients to access api's

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sql_app.main import router as api_router
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",
+    "https://iamabhinav03.github.io",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routes from api_routes.main.router
 app.include_router(api_router, prefix="/api")
