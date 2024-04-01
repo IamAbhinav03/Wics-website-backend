3768faa7982cc4d21c0bf682258216e8a05bec7b
Abhinav M Hari <abhinav03m@gmail.com>
2024-04-01 13:51:14 +0530

    Changed code to connect to production database from local test database.

--- sql_app/database.py
+++ sql_app/database.py
@@ -8,10 +8,14 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
 
-# sqlite db for testing
-load_dotenv(".env.development.local")
-DATABASE_URL = os.environ["TEST_DATABASE_URL"]
-# DATABASE_URL = "postgresql://default:jSJg6x8vfXuL@ep-round-mouse-a1oo2jy0-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+# env for testing
+# load_dotenv(".env.development.local")
+# DATABASE_URL = os.environ["TEST_DATABASE_URL"]
+
+# env for production
+load_dotenv(".env")
+DATABASE_URL = os.environ["DATABASE_URL"]
+
 
 # connect_args only required for sqlite
 # engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
